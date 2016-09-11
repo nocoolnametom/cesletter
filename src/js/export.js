6eb8147df51a5123ed67701217b69b326a4f8c4d
Tom Doggett <nocoolnametom@gmail.com>
2016-09-10 18:58:53 -0700

    Fixing issues outputing a jsnext:main module.

--- src/js/export.js
+++ src/js/export.js
@@ -5,3 +5,9 @@ import index from '../../dist/partials/index';
 export const initScript = initializeCesLetterScripts;
 export const indexHtml = index;
 export const offlineHtml = offline;
+export default {
+  initScript: initializeCesLetterScripts,
+  indexHtml: index,
+  offlineHtml: offline,
+};
+
